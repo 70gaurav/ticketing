fdc5f9f8d753293382eaa27c1bcde35ee7bb2b7f
Gaurav <gk17265@gmail.com>
2023-10-05 11:21:50 +0530

    auth

---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,15 @@
+import {Request , Response , NextFunction} from 'express'
+import { DatabaseConnectionError } from '../errors/database-connection-error'
+import { RequestValidationError } from '../errors/request-validation-error'
+export const errorHandler = (err:Error , req: Request, res: Response, next: NextFunction) => {
+if(err instanceof RequestValidationError){
+    console.log('handling this as a request validation error')
+}
+if(err instanceof DatabaseConnectionError){
+    console.log('handling this as a db conection error')
+}
+
+res.status(400).send({
+    message: err.message
+})
+}
