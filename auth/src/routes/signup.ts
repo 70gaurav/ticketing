fb9576bc4eecc2dfeba5ff4772e2070dbf750264
Gaurav <gk17265@gmail.com>
2023-10-10 10:23:11 +0530

    cookie and token

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from 'express-validator'
 import User from "../models/user";
+import jwt from 'jsonwebtoken'
 import { RequestValidationError } from "../errors/request-validation-error";
+import { BadRequestError } from "../errors/bad-request-error";
 
 
 const router = express.Router();
@@ -27,12 +29,23 @@ router.post('/api/users/signup', [
        const existingUser = await User.findOne({email})
 
        if(existingUser){
-        console.log('Email in use')
-        return res.send({})
+        throw new BadRequestError('Email in use')
        }
 
        const user = User.build({email, password})
        await user.save()
+
+       //generate token 
+
+       const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email
+       }, 'asdf')
+
+       //store it on session object
+       req.session = {
+        jwt: userJwt
+       }
 
        res.status(201).send(user)
     })
