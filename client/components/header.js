18b78a21a5ce518df878346fb249b2cb571de305
Gaurav <gk17265@gmail.com>
2023-10-20 11:28:12 +0530

    header links

--- client/components/header.js
+++ client/components/header.js
@@ -1,15 +1,15 @@
 import Link from "next/link";
 
 export default ({ currentUser }) => {
-    const links = [
-        !currentUser && {label: 'Sign Up' , href: '/auth/signup'},
-        !currentUser && {label: 'Sign In' , href: '/auth/signin'},
-        currentUser && {label: 'Sign Out' , href: '/auth/signout'},
-    ]
-    .filter(linkConfig => linkConfig)
-    .map(({label , href})=> {
-        return <li key={href} className="nav-item"><Link className="navbar-brand" href={href}>{label}</Link></li>
-    })
+    // const links = [
+    //     !currentUser && {label: 'Sign Up' , href: '/auth/signup'},
+    //     !currentUser && {label: 'Sign In' , href: '/auth/signin'},
+    //     currentUser && {label: 'Sign Out' , href: '/auth/signout'},
+    // ]
+    // .filter(linkConfig => linkConfig)
+    // .map(({label , href})=> {
+    //     return <li key={href} className="nav-item"><Link className="navbar-brand" href={href}>{label}</Link></li>
+    // })
 
 
   return (
@@ -20,7 +20,7 @@ export default ({ currentUser }) => {
 
       <div className="d-flex justify-content-end">
         <ul className="nav d-flex align-items-center">
-            {currentUser ? 'Sign out' : 'Sign in/up'}
+            {currentUser ? <Link href={'/auth/signout'}>signout</Link> : 'Sign in/up'}
         </ul>
       </div>
     </nav>
