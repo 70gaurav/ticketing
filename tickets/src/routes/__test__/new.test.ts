6924595c2060c951487103f957fac38ec21defac
Gaurav <gk17265@gmail.com>
2023-10-23 10:53:48 +0530

    ticket test

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,18 @@
+import request from 'supertest'
+import app from '../../app'
+
+it('has a route handler listening to /api/tickets for post post requests', async () => {
+    
+})
+it('can only be accessed if the user is signed in', async () => {
+
+})
+it('it return an error if an invalid tittle is provided', async () => {
+
+})
+it('it return an error if an invalid price is provided', async () => {
+
+})
+it('it creates a ticket with valid inputs', async () => {
+
+})
