c13a4ec8bac8c822276b1d45c261289d3f2acb64
Gaurav <gk17265@gmail.com>
2023-12-11 15:58:20 +0530

    nats

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,5 +1,5 @@
 import nats from 'node-nats-streaming'
-
+import { TicketCreatedPblisher } from './events/ticket-created-publisher'
 console.clear()
 const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222'
@@ -9,7 +9,7 @@ stan.on('connect' , () => {
     console.log('publisher connected to NATS')
 
     const data = JSON.stringify({
-        id:'123',
+        id:'123',   
         title: 'semi-final',
         price: 20
     })
@@ -17,4 +17,33 @@ stan.on('connect' , () => {
     stan.publish('ticket:created', data, () => {
         console.log('Event published')
     })
+
+    const publisher = new TicketCreatedPblisher(stan)
+    publisher.publish({
+        id: '123',
+        title: 'concert',
+        price: 30
+    })
 })
+
+
+
+// import nats from 'node-nats-streaming';
+
+// const stan = nats.connect('ticketing', 'abc', {
+//   url: 'http://localhost:4222',
+// });
+
+// stan.on('connect', () => {
+//   console.log('Publisher connected to NATS');
+
+//   const data = JSON.stringify({
+//     id: '123',
+//     title: 'concert',
+//     price: 20,
+//   });
+
+//   stan.publish('ticket:created', data, () => {
+//     console.log('Event published');
+//   });
+// });
